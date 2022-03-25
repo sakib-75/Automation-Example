9ccb3178890a531f4db09396f4d1e91cdce95b1f
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 22:35:23 +0600

    KeyboardEvent

---
+++ src/test/java/test_1/KeyboardEvent.java
@@ -0,0 +1,29 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+public class KeyboardEvent extends BaseClass {
+    public static String baseUrl = "http://automationpractice.com/index.php";
+
+    @Test
+    public static void KeyboardEventCheck() throws InterruptedException {
+
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver, 5);
+
+        WebElement e = driver.findElement(By.xpath("//input[@id='search_query_top']"));
+        e.sendKeys(Keys.chord(Keys.SHIFT, "tedtalk"));
+
+        e.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        e.sendKeys(Keys.chord(Keys.ENTER));
+
+        Thread.sleep(2000);
+
+    }
+
+}
