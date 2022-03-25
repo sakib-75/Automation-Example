f6e145a9f27ec80c617f0b6d5d8efb4e1c482ff5
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 16:00:09 +0600

    CheckBox_RadioButton

---
+++ src/test/java/test_1/CheckBox_RadioButton.java
@@ -0,0 +1,31 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+// Selection of Radio button and Check box in Frames:
+public class CheckBox_RadioButton extends BaseClass {
+
+    public static String baseUrl = "https://jqueryui.com/checkboxradio";
+
+    @Test
+    public static void CheckBoxAndRadioButtonInFrame() throws InterruptedException {
+
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver,5);
+
+        // Switch to Frame
+        driver.switchTo().frame(0);
+
+        driver.findElement(By.xpath("(//span[contains(@class,'checkboxradio')])[5]")).click();
+        driver.findElement(By.xpath("//body/div[1]/fieldset[1]/label[3]")).click();
+        driver.findElement(By.xpath("//body[1]/div[1]/fieldset[2]/label[4]/span[1]")).click();
+
+        // Back to page
+        driver.switchTo().defaultContent();
+
+    }
+
+}
