9646b33f18fd57f67cbd4cdae093f342563e5869
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 16:44:22 +0600

    NavigatePage

---
+++ src/test/java/test_1/NavigatePage.java
@@ -0,0 +1,28 @@
+package test_1;
+
+import base.BaseClass;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+public class NavigatePage extends BaseClass {
+
+    public String baseUrl = "https://www.w3schools.com";
+
+    @Test
+    public void refreshPage() throws InterruptedException {
+
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver, 5);
+
+        driver.navigate().to("http://www.google.com");
+        Thread.sleep(1000);
+        driver.navigate().back();
+        Thread.sleep(1000);
+        driver.navigate().forward();
+        Thread.sleep(1000);
+        driver.navigate().refresh();
+        Thread.sleep(1000);
+
+    }
+
+}
