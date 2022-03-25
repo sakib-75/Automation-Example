ce307ea93fe624cdd9bfde391c984f3963913a24
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 22:46:44 +0600

    AlertPopupWindow

---
+++ src/test/java/test_1/AlertPopupWindow.java
@@ -0,0 +1,26 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+public class AlertPopupWindow extends BaseClass {
+    public String baseUrl = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert";
+
+    @Test
+    public void alertHandlingCheck() throws InterruptedException {
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver, 5);
+
+        WebElement b=driver.findElement(By.name("iframeResult"));
+        driver.switchTo().frame(b);
+        driver.findElement(By.xpath("//button[text()='Try it']")).click();
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+        //driver.switchTo().alert().dismiss();
+
+    }
+
+}
