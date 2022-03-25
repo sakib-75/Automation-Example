9d87406562f35d00313040b6c14b0457c71b41c3
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 16:38:30 +0600

    SelectDropDown

---
+++ src/test/java/test_1/SelectDropDown.java
@@ -0,0 +1,39 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+public class SelectDropDown extends BaseClass {
+    public static String baseUrl = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+
+    @Test
+    public static void selectingValueFromDropdown() throws InterruptedException {
+
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver, 5);
+
+        driver.switchTo().frame("iframeResult");
+
+        WebElement el = driver.findElement(By.name("cars"));
+        Select s = new Select(el);
+        s.selectByIndex(0);
+        actions.keyDown(Keys.CONTROL)
+                .click(s.getOptions().get(3))
+                .click(s.getOptions().get(1))
+                .keyUp(Keys.CONTROL)
+                .build().perform();
+
+        //deselectAll( ):
+        //deselectByValue( )
+        //deselectByVisibleText( ):
+        //getAllSelectedOptions( ):
+
+        driver.switchTo().defaultContent();
+    }
+
+}
