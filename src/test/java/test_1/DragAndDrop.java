7540a22d71057bd9df3ef50ef60d46d5c21e2759
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 17:21:57 +0600

    Drag and Drop

---
+++ src/test/java/test_1/DragAndDrop.java
@@ -0,0 +1,29 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.CommonUtility;
+
+public class DragAndDrop extends BaseClass {
+    public static String baseUrl = "https://jqueryui.com/droppable";
+
+    @Test
+    public static void dragAndDropCheck() {
+
+        driver.get(baseUrl);
+        CommonUtility.implicitWait(driver, 5);
+
+        // Drag and Drop
+        driver.switchTo().frame(0);
+
+        WebElement e1 = driver.findElement(By.id("draggable"));
+        WebElement e2 = driver.findElement(By.id("droppable"));
+
+        actions.dragAndDrop(e1, e2).build().perform();
+        driver.switchTo().defaultContent();
+
+    }
+
+}
