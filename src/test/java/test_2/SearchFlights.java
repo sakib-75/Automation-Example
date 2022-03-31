d2ffaefbb873bbd8352a3c46a33edb733142894c
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-31 15:28:28 +0600

    Search flights update

--- src/test/java/test_2/SearchFlights.java
+++ src/test/java/test_2/SearchFlights.java
@@ -2,6 +2,7 @@ package test_2;
 
 import base.BaseClass;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utilities.CommonUtility;
@@ -13,7 +14,7 @@ public class SearchFlights extends BaseClass {
     public String baseUrl = "https://www.yatra.com/";
 
     @Test
-    public void s() throws InterruptedException {
+    public void searchFlights() throws InterruptedException {
         driver.get(baseUrl);
         CommonUtility.implicitWait(driver, 5);
 
@@ -21,20 +22,26 @@ public class SearchFlights extends BaseClass {
         WebElement depart_from_inp = driver.findElement(By.xpath("//input[@id='BE_flight_origin_city']"));
         depart_from_inp.click();
         String depart_city = "Chennai (MAA)";
+        selectCity(depart_city);
+        Thread.sleep(1000);
 
-        List<WebElement> all_depart_option = driver.findElements(By.xpath("//div[@class='viewport']//div[1]/li"));
-        for (WebElement city : all_depart_option) {
-            if (city.getText().contains(depart_city)) {
+        // Select going to
+        WebElement going_to_inp = driver.findElement(By.xpath("//input[@id='BE_flight_arrival_city']"));
+        going_to_inp.click();
+        String going_to_city = "Goa (GOI)";
+        selectCity(going_to_city);
+        Thread.sleep(1000);
+
+    }
+
+    public static void selectCity(String city_name){
+        List<WebElement> all_city = driver.findElements(By.xpath("//div[@class='viewport']//div[1]/li"));
+        for (WebElement city : all_city) {
+            if (city.getText().contains(city_name)) {
                 city.click();
                 break;
             }
         }
-        Thread.sleep(2000);
-
-        // Select going to
-        WebElement going_to_inp = driver.findElement(By.xpath("//input[@id='BE_flight_arrival_city']"));
-        
-
     }
 
 }
