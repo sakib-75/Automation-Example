a1758fe0f92d9e650ca800b43d67968d699f3897
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-06 13:30:32 +0600

    Update LoginPage_POM.java

--- src/test/java/test_3/LoginPage_POM.java
+++ src/test/java/test_3/LoginPage_POM.java
@@ -1,12 +1,39 @@
 package test_3;
 
 import base.PageDriver;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import static utilities.CommonUtility.screenshotForAllure;
+import static utilities.CommonUtility.sendText;
 
 public class LoginPage_POM {
 
     public LoginPage_POM() {
         PageFactory.initElements(PageDriver.getCurrentDriver(), this);
     }
+
+    @FindBy(name = "email")
+    WebElement emailInput;
+
+    @FindBy(name = "password")
+    WebElement passwordInput;
+
+    @FindBy(xpath = "//button[normalize-space()='LOGIN']")
+    WebElement loginBtn;
+
+    @Step("Provide an Email & Password input")
+    public void login(String email, String password) {
+        sendText(emailInput, email);
+        sendText(passwordInput, password);
+        screenshotForAllure("Login information");
+    }
+
+    public void loginButtonClick() {
+        loginBtn.click();
+    }
+
 
 }
