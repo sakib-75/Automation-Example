896ea9aa07c3884810d964150a06252d3dcbd726
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-21 20:03:36 +0600

    Update CheckoutStepOne_POM.java

--- src/test/java/test_4/CheckoutStepOne_POM.java
+++ src/test/java/test_4/CheckoutStepOne_POM.java
@@ -1,12 +1,42 @@
 package test_4;
 
 import base.PageDriver;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import static utilities.CommonUtility.*;
 
 public class CheckoutStepOne_POM {
 
     public CheckoutStepOne_POM() {
         PageFactory.initElements(PageDriver.getCurrentDriver(), this);
+    }
+
+    @FindBy(xpath = "//input[@id='first-name']")
+    WebElement firstNameInput;
+
+    @FindBy(xpath = "//input[@id='last-name']")
+    WebElement lastNameInput;
+
+    @FindBy(xpath = "//input[@id='postal-code']")
+    WebElement zipCodeInput;
+
+    @FindBy(id = "continue")
+    WebElement continueButton;
+
+    @FindBy(id = "cancel")
+    WebElement cancelButton;
+
+    @Step("Checkout step one, fill all information")
+    public void checkoutStepOne(String first_name, String last_name, String zip_code) {
+        sendText(firstNameInput, first_name);
+        sendText(lastNameInput, last_name);
+        sendText(zipCodeInput, zip_code);
+        screenshotForAllure("Checkout step one, input info");
+        continueButton.click();
+
     }
 
 }
