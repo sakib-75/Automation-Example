f64a98f27d939427f37457dde416c54cc2a4328c
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-07-09 16:59:02 +0600

    Create FileUpload.java

---
+++ src/test/java/test_1/FileUpload.java
@@ -0,0 +1,27 @@
+package test_1;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class FileUpload extends BaseClass {
+
+    @Test
+    public void file_upload() throws InterruptedException {
+
+        driver.get("https://www.monsterindia.com/seeker/registration");
+
+        File file_dir = new File("src/test/java/resources");
+        File file = new File(file_dir, "image.jpg");
+
+        WebElement browse_file = driver.findElement(By.id("file-upload"));
+        browse_file.sendKeys(file.getAbsolutePath());
+        Thread.sleep(2000);
+        System.out.println("File is Uploaded Successfully");
+
+    }
+
+}
